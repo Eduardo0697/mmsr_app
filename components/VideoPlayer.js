945183bc31fc7d8264cf01a044dffe0e4922853f
Eduardo Hernandez <eduardohdz0697@hotmail.com>
2023-01-13 14:47:50 +0100

    Component to play youtube videos based in the video id

---
+++ components/VideoPlayer.js
@@ -0,0 +1,28 @@
+import YouTube from 'react-youtube';
+import styles from "./VideoPlayer.module.css"
+
+export default function VideoPlayer({songToPlay}) {
+
+    const opts = {
+        // height: '390',
+        width: '900',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+          rel: 0,
+          modestbranding: 1
+        },
+      };
+
+      function _onReady(event) {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+      }
+
+    return(
+        
+        <div className={styles.videoContainer}>
+            <YouTube videoId={songToPlay}  opts={opts} onReady={_onReady}/>  
+        </div>
+    )
+}
